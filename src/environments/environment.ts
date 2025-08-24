71405b2a114d451737f1bdbbcf176bfc7964192a
Jennifer Arabel <119452050+JenniArabel@users.noreply.github.com>
2025-08-24 18:30:11 -0300

    Update environment.ts
    Confidential information goes here

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDjyAWMtY-9_55Xc7HOcF8sKmr67g5TYag",
-    authDomain: "appdemo-fotos.firebaseapp.com",
-    projectId: "appdemo-fotos",
-    storageBucket: "appdemo-fotos.firebasestorage.app",
-    messagingSenderId: "400476620449",
-    appId: "1:400476620449:web:d0a64f55f9a50d51c5d2e0"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 }
